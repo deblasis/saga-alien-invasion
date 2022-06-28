2f64b33e4e03a9ebe7d6d6383f6682a5b72db45b
Alessandro De Blasis <alex@deblasis.net>
2022-06-28 10:11:27 +0100

    feat(logging): scaffolding

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/deblasis/saga-alien-invasion/cmd"
+import (
+	"os"
+
+	"github.com/deblasis/saga-alien-invasion/cmd"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	cmd.Execute()
 }
