bfb2a1a599c2136d82ded611f5acb7517ddb61a4
Alessandro De Blasis <alex@deblasis.net>
2022-06-28 10:14:38 +0100

    feat(CLI): argument parsing

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,34 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "invasion [number of aliens invading]",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("the number of aliens invading the world must be specified")
+		}
+		var n int
+		n, err := strconv.Atoi(args[0])
+		if err != nil || n <= 0 {
+			return fmt.Errorf("the number of aliens must be *checks notes* a positive number! received: %s", args[0])
+		}
+
+		return nil
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		cmd.Println("DOING SOMETHING")
+
+		aliensCount, err := strconv.Atoi(args[0])
+		if err != nil {
+			return err
+		}
+		cmd.Printf("DOING SOMETHING with aliensCount: %d\n", aliensCount)
 		return nil
 	},
 }
